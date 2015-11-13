d687caece676ed111b7f2a655baaea7818098554
klriutsa <hiroya.kurushima@litalico.co.jp>
2015-11-13 19:39:08 +0900

    Step 2

--- klriutsa/vending_machine.rb
+++ klriutsa/vending_machine.rb
@@ -1,3 +1,22 @@
-def init
-  
+class VendingMachine
+  def initialize
+    @money = 0
+    @available_money = [10, 50,100, 500, 1000]
+    @juices = Hash.new { |h,k| h[k] = {} }
+    @juices[:cola]= {name: "コーラ", price: 120, quantity: 5}
+  end
+
+  def insert(money)
+    if @available_money.include?(money)
+      @money += money
+      p "投入金額#{@money}"
+    else
+      p "#{@money}円を返却"
+      p "投入金額#{@money}"
+    end
+  end
+
+  def create(juice, name, price, quantity)
+    @juices[juice.to_sym] = {name: name, price: price, quantity: quantity}
+  end
 end
