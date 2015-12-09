4c2fdcf30e95e6c5d858076bb4d327a929e58c3e
klriutsa <hiroya.kurushima@litalico.co.jp>
2015-12-09 17:12:01 +0900

    Modify : vm

--- klriutsa/vending_machine.rb
+++ klriutsa/vending_machine.rb
@@ -1,30 +1,43 @@
 class VendingMachine
   def initialize
     @money = 0
-    @available_money = [10, 50,100, 500, 1000]
-    @juices = Hash.new { |h,k| h[k] = {} }
-    @juices[0]= {name: "コーラ", price: 120, quantity: 5}
+    @available_money = [10, 50, 100, 500, 1000]
+    @drinks = []
   end
 
   def insert(money)
     if @available_money.include?(money)
       @money += money
-      p "投入金額#{@money}"
     else
-      p "#{@money}円を返却"
-      p "投入金額#{@money}"
     end
   end
 
-  def create(name, price, quantity)
-    @juices[@juices.length] = {name: name, price: price, quantity: quantity}
+  def create(drink, quantity)
+    @drinks << {drink: drink, quantity: quantity}
+  end
+
+  def return
+    @money = 0
+  end
+
+  def buy()
   end
 
   def list
-    @juices.each do |id, juice|
-      if juice[id][:price].to_i < @money && juice[id][:quantity] > 0
-        p juice[id][:name]
-      end
-    end
+    @drinks
+  end
+
+  private
+
+  def self.create_cola
+    cola = Drink.new("コーラ",120)
+    self.create(cola,5)
+  end
+end
+
+class Drink
+  def initialize(name, price)
+    @name = name
+    @price = price
   end
 end
